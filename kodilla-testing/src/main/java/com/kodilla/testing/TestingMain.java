f3e6bc3315d70c3051cb7f24c7906e6cbc8be145
Flymanos <karol.mucha17@gmail.com>
2017-10-10 20:20:44 +0200

    6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        System.out.println("Test Calculator");
+        Calculator calculator = new Calculator();
+
+        System.out.println("Test method add");
+        if (calculator.add(2, 2) == 4){
+            System.out.println("Test passed.");
+        } else{
+            System.out.println("Something went wrong here!");
+        }
+
+        System.out.println("Test method subtract");
+        if (calculator.subtract(2, 2) == 0){
+            System.out.println("Test passed.");
+        } else{
+            System.out.println("Something went wrong here!");
+        }
     }
 }
