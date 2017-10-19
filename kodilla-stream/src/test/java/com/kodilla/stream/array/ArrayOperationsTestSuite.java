50d3920f4a48109ddb481f2bfb7fabd6db119b21
Flymanos <karol.mucha17@gmail.com>
2017-10-19 22:00:24 +0200

    7.5.1

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -3,14 +3,11 @@ package com.kodilla.stream.array;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Random;
-
 public class ArrayOperationsTestSuite {
 
     @Test
     public void testGetAverage(){
         //given
-        Random random = new Random();
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
         //when
         double result = ArrayOperations.getAverage(numbers);
