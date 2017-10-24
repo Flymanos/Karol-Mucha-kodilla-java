8372442dcc79438ea8e7dade6f97d1c75905e7e7
Flymanos <karol.mucha17@gmail.com>
2017-10-24 17:30:17 +0200

    8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -7,16 +7,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class FileReader {
+public class FileReader{
 
-    public void readFile(){
+    public void readFile() throws FileReaderException{
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("names.txt").getFile());
         try {
             Stream<String> fileLines = Files.lines(Paths.get(file.getPath()));
             fileLines.forEach(System.out::println);
         } catch (IOException e){
-            System.out.println(e);
+            throw new FileReaderException();
         }
     }
 
