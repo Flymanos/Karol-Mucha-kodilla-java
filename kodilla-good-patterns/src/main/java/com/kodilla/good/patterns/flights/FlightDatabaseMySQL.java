66f6edf4e8b512651e32091a93d7bd3b46690c73
Flymanos <karol.mucha17@gmail.com>
2017-10-31 21:27:04 +0100

    9.5.3

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/FlightDatabaseMySQL.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/flights/FlightDatabaseMySQL.java
@@ -3,7 +3,7 @@ package com.kodilla.good.patterns.flights;
 import java.util.HashSet;
 
 public class FlightDatabaseMySQL implements FlightDatabase{
-    HashSet<Flight> flights = new HashSet<>();
+    private HashSet<Flight> flights = new HashSet<>();
 
     public HashSet<Flight> getFlights() {
         return flights;
